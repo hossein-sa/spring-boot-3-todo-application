c052a6c91a075f14e464c2e4ebd87d3c45c4b881
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-06-11 15:19:23 +0330

    add edit-todo-item.html

--- src/main/java/ir/hsadeghi/springboot3todoapplication/controllers/TodoFormController.java
+++ src/main/java/ir/hsadeghi/springboot3todoapplication/controllers/TodoFormController.java
@@ -1,9 +1,17 @@
 package ir.hsadeghi.springboot3todoapplication.controllers;
 
+import ir.hsadeghi.springboot3todoapplication.models.TodoItem;
 import ir.hsadeghi.springboot3todoapplication.services.TodoItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,7 +19,35 @@ public class TodoFormController {
     private final TodoItemService todoItemService;
 
     @GetMapping("/create-todo")
-    public String showCreateForm() {
+    public String showCreateForm(TodoItem todoItem) {
         return "new-todo-item";
+    }
+
+    @PostMapping("/todo")
+    public String createTodoItem(@Valid TodoItem todoItem, BindingResult result, Model model) {
+        TodoItem item = new TodoItem();
+        item.setDescription(todoItem.getDescription());
+        item.setIsComplete(todoItem.getIsComplete());
+
+        todoItemService.save(todoItem);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id, Model model) {
+        TodoItem todoItem = todoItemService
+                .getById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+        todoItemService.delete(todoItem);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id,Model model) {
+        TodoItem todoItem = todoItemService
+                .getById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+        model.addAttribute("todo", todoItem);
+        return "edit-todo-item";
     }
 }
